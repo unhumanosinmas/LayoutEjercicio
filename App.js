46aa20d392755edae9b4f751bd2049172322e64e
Diego Enrique Hernández Cholotío <104159636+unhumanosinmas@users.noreply.github.com>
2022-05-11 19:57:30 -0600

    a

--- App.js
+++ App.js
@@ -7,16 +7,14 @@
  */
 
 import React from 'react';
-import {
-  SafeAreaView
-} from 'react-native';
-
+import {SafeAreaView} from 'react-native';
+import Main from './src/views/Main';
 
 const App = () => {
 
   return (
     <SafeAreaView>
-  
+      <Main />
     </SafeAreaView>
   );
 };
