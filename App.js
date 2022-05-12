d15700c7b982107276d515184c147c634e4345a1
Diego Enrique Hernández Cholotío <104159636+unhumanosinmas@users.noreply.github.com>
2022-05-11 19:20:34 -0600

    Commit 1
    Creacion base de la app

---
+++ App.js
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView
+} from 'react-native';
+
+
+const App = () => {
+
+  return (
+    <SafeAreaView>
+  
+    </SafeAreaView>
+  );
+};
+
+export default App;
